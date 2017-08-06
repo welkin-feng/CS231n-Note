9fa3290c01c5c84637ac85cda23c0deb506b299b
Welkin <382523558@qq.com>
2017-08-06 16:26:44 +0800

    Create scipy_tutorial.py

---
+++ Python Tutorial/scipy_tutorial.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Project Name:   Python Tutorial 
+
+File Name:  scipy_tutorial.py
+
+"""
+
+__author__ = 'Welkin'
+__date__ = '2017/8/6 16:25'
